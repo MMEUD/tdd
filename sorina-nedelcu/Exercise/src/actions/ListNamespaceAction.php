06dd32fad1082ff9e859914f0084e6bd49aa446e
Sorina Nedelcu <s.nedelcu@moodmedia.com>
2013-04-24 18:25:22 +0300

    TDD Exercise 1
    (work in progress)

---
+++ sorina-nedelcu/Exercise/src/actions/ListNamespaceAction.php
@@ -0,0 +1,48 @@
+<?php
+require_once 'Action.php';
+require_once '/ContextSingleton.php';
+require_once '/util/NamespaceObject.php';
+require_once '/util/Property.php';
+
+
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Sorina Nedelcu
+ * Date: 4/22/13
+ * Time: 3:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class ListNamespaceAction implements Action {
+
+  private $context;  //ContextSingleton
+
+  public function __construct() {
+    $this->context = ContextSingleton::getInstance();
+  }
+
+  public function doAction($params) {
+    $msg =  "\n";
+    if(count($params)<=1) {
+      // get all namespaces
+      $allNamespaces = $this->context->getNamespace();
+      ksort($allNamespaces);
+
+      foreach($allNamespaces as $param => $aNamespace) {
+        $msg = $msg . " " . $aNamespace->getName() . "\n";;
+        $properties = $aNamespace->getProperties();
+        for ($j=0; $j<count($properties); $j++) {
+           $msg = $msg . "  " . $aNamespace->getName() . " : " . $properties[$j]->getName() . "=" . $properties[$j]->getValue() .  "\n";
+        }
+      }
+    } else {
+      // get namespace
+      $currentNamespace = $this->context->getNamespaceObj($params[1]);
+      $properties = $currentNamespace->getProperties();
+      for ($j=0; $j<count($properties); $j++) {
+         $msg = $msg . "  " . $currentNamespace->getName() . " : " . $properties[$j]->getName() . "=" . $properties[$j]->getValue() .  "\n";
+      }
+    }
+    $msg = $msg . "\n";
+    return $msg;
+  }
+}
