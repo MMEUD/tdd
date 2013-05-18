d551c0a8ffe0e5e1f610304df1e743de74cef7f9
Ancuta Gheorghe <a.gheorghe@gmail.com>
2013-05-18 22:58:30 +0300

    RentingMovies 1.0.5
    Removing local thisAmount by using Replace temp with query

--- ancuta-gheorghe/RentingMovies/src/Customer.java
+++ ancuta-gheorghe/RentingMovies/src/Customer.java
@@ -31,18 +31,15 @@ public class Customer {
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()){
-            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-
-            thisAmount = each.getCharge();
 
             frequentRenterPoints++;
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
                 && each.getDaysRented() > 1) frequentRenterPoints++;
 
             result += "\t" + each.getMovie().getTitle() + "\t" +
-                    String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+                    String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
         }
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) +
