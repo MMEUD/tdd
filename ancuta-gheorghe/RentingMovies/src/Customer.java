f423d4d00d9c3275f99f7c2408b00e36a84dbd01
Ancuta Gheorghe <a.gheorghe@gmail.com>
2013-05-18 22:53:45 +0300

    RentingMovies 1.0.4
    Moving&changing amountFor() from Customer to getCharge() in Rental

--- ancuta-gheorghe/RentingMovies/src/Customer.java
+++ ancuta-gheorghe/RentingMovies/src/Customer.java
@@ -34,7 +34,7 @@ public class Customer {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
 
-            thisAmount = amountFor(each);
+            thisAmount = each.getCharge();
 
             frequentRenterPoints++;
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
@@ -50,23 +50,4 @@ public class Customer {
         return result;
     }
 
-    private double amountFor(Rental aRental) {
-        double result = 0;
-        switch (aRental.getMovie().getPriceCode()){
-            case Movie.REGULAR :
-                result += 2;
-                if (aRental.getDaysRented() > 2)
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 2;
-                break;
-            case Movie.CHILDREN:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
-    }
 }
