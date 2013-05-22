e8d88546e266d8976d982536a5b352785a34c30e
Radu Obreja <r.obreja@moodmedia.com>
2013-05-22 10:10:26 +0300

    Include basic calls

--- radu-obreja/Studio/src/com/iolma/tests/H264Recorder.java
+++ radu-obreja/Studio/src/com/iolma/tests/H264Recorder.java
@@ -1,13 +1,33 @@
 package com.iolma.tests;
 
+import com.iolma.studio.gui.VideoPanel;
+import com.iolma.studio.log.ConsoleLogger;
+import com.iolma.studio.log.ILogger;
+import com.iolma.studio.process.capture.VideoCapture;
+import com.iolma.studio.process.tick.Ticker;
+
 public class H264Recorder {
 
 	public static void main(String[] args) {
 		
-		String filename = "h264_sample.mp4";
+		// Create logger
+		ILogger logger = new ConsoleLogger();
 		
-		
+		// Create ticker
+		Ticker videoTicker = new Ticker(1);
+		videoTicker.setLogger(logger);
 
+		// Create video capture
+		VideoCapture videoCapture = new VideoCapture();		
+		videoCapture.addInput(videoTicker);
+		
+		// Create gui
+		VideoPanel videoPanel = new VideoPanel();
+		videoPanel.addInput(videoCapture);
+		
+		// Start ticker
+		videoTicker.startup();
+		
 	}
 
 }
