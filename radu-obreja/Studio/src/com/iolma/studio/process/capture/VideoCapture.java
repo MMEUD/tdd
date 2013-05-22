b41a05ab5adf2d80068e3c51738943c2988db160
Radu Obreja <r.obreja@moodmedia.com>
2013-05-22 10:10:02 +0300

    Add ticker

---
+++ radu-obreja/Studio/src/com/iolma/studio/process/capture/VideoCapture.java
@@ -0,0 +1,13 @@
+package com.iolma.studio.process.capture;
+
+import com.iolma.studio.process.BasicProcess;
+
+public class VideoCapture extends BasicProcess {
+
+	public void execute() {
+		
+		
+		fireNextProcess();
+	}
+
+}
