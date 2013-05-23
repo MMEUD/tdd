0d1179652f72fcfdd05c63b0fd53330eadfc3ae5
Radu Obreja <r.obreja@moodmedia.com>
2013-05-23 17:47:45 +0300

    Add audio / video support

--- radu-obreja/Studio/src/com/iolma/studio/process/Frame.java
+++ radu-obreja/Studio/src/com/iolma/studio/process/Frame.java
@@ -1,5 +1,27 @@
 package com.iolma.studio.process;
 
+import com.xuggle.xuggler.IAudioSamples;
+import com.xuggle.xuggler.IVideoPicture;
+
 public class Frame implements IFrame {
+	
+	private IVideoPicture picture = null;
+	private IAudioSamples audioSamples = null;
+
+	public void setVideoPicture(IVideoPicture picture) {
+		this.picture = picture;
+	}
+	
+	public IVideoPicture getVideoPicture() {
+		return picture;
+	}
+
+	public void setAudioSamples(IAudioSamples audioSamples) {
+		this.audioSamples = audioSamples;
+	}
+
+	public IAudioSamples getAudioSamples() {
+		return audioSamples;
+	}
 
 }
