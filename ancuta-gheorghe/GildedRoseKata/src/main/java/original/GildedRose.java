0e5a89556c56690bbc31202af35e53df3963f63c
Ancuta Gheorghe <a.gheorghe@gmail.com>
2013-06-01 19:49:40 +0300

    GildedRoseKata 1.0.1

---
+++ ancuta-gheorghe/GildedRoseKata/src/main/java/original/GildedRose.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package main.java.original;
+
+/**
+ * @author ancuta
+ *
+ */
+public class GildedRose {
+
+	public static void main(String[] args) {
+		
+    	 System.out.println("OMGHAI!"); 
+		 Inventory inventory = new Inventory();
+         inventory.updateQuality();
+	}
+		
+}
