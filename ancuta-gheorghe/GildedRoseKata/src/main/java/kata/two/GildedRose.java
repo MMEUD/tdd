efd4b41b89681da7fdf0ed06a93260814f563324
Ancuta Gheorghe <a.gheorghe@gmail.com>
2013-06-02 11:32:42 +0300

    GildedRoseKata 1.1.1

---
+++ ancuta-gheorghe/GildedRoseKata/src/main/java/kata/two/GildedRose.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package main.java.kata.two;
+
+/**
+ * @author ancuta
+ *
+ */
+public class GildedRose {
+
+	public static void main(String[] args) {
+		
+    	 Inventory inventory = new Inventory();
+         inventory.updateQuality();
+         System.out.println(inventory.toString());
+	}
+		
+}
---
+++ ancuta-gheorghe/GildedRoseKata/src/tests/main/java/kata/two/ItemKataTwo.java
@@ -0,0 +1,24 @@
+package tests.main.java.kata.two;
+
+import static org.junit.Assert.*;
+
+import main.java.kata.two.Inventory;
+
+import org.junit.Test;
+
+public class ItemKataTwo {
+
+	@Test public void  
+	check_output_for_updateQuality(){
+		String output = "+5 Dexterity Vest 1 19\n" +
+					"Aged Brie 1 1\n" +
+					"Elixir of the Mongoose 1 6\n" +
+					"Sulfuras, Hand of Ragnaros 1 80\n" +
+					"Backstage passes to a TAFKAL80ETC concert 1 21\n" +
+					"Conjured Mana Cake 1 5\n";
+		Inventory inventory = new Inventory();
+        inventory.updateQuality();
+		assertEquals(output, inventory.toString());
+	}
+
+}
