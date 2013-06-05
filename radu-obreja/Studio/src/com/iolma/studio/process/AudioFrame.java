7e31bbfa5cdb7f11f63502321764b8ea9ac1b34c
Radu Obreja <r.obreja@moodmedia.com>
2013-06-05 15:37:08 +0300

    Split into 2 different projects : registrar and videocall

--- radu-obreja/Studio/src/com/iolma/studio/process/AudioFrame.java
+++
@@ -1,5 +0,0 @@
-package com.iolma.studio.process;
-
-public class AudioFrame extends Frame {
-
-}
--- radu-obreja/Studio/src/com/iolma/studio/process/IServer.java
+++
@@ -1,9 +0,0 @@
-package com.iolma.studio.process;
-
-public interface IServer {
-
-	public void startup();
-	
-	public void shutdown();
-	
-}
