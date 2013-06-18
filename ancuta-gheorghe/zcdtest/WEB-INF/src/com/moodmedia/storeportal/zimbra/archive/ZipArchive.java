a89bb760555ac648718412c1014fb25836441b62
Ancuta Gheorghe <a.gheorghe@gmail.com>
2013-06-18 22:44:25 +0300

    ZimbraRest 1.0.17

--- ancuta-gheorghe/zcdtest/WEB-INF/src/com/moodmedia/storeportal/zimbra/archive/ZipArchive.java
+++ ancuta-gheorghe/zcdtest/WEB-INF/src/com/moodmedia/storeportal/zimbra/archive/ZipArchive.java
@@ -20,49 +20,6 @@ import com.moodmedia.storeportal.zimbra.connection.CustomRequest;
  */
 public class ZipArchive {
 	
-	public static void unzip() {
-		String archive = "D:\\zcs\\ancuta_263.zip";
-		File baseFolder= new File("D:\\zcs\\ancuta_263");
-	    FileInputStream fin;
-	    try {
-	      fin = new FileInputStream(archive);
-	      ZipInputStream zin = new ZipInputStream(fin);
-	      ZipEntry ze = null;
-	      while ((ze = zin.getNextEntry()) != null) {
-	          File destinationFile = new File(baseFolder, ze.getName());
-	          unpackEntry(destinationFile, zin);
-	      }
-	      zin.close();
-	    } catch (FileNotFoundException e) {
-	      e.printStackTrace();
-	    } catch (IOException e) {
-	      e.printStackTrace();
-	    }
-	  }
-
-	  private static void unpackEntry(File destinationFile, ZipInputStream zin) {
-	    createParentFolder(destinationFile);
-	    FileOutputStream fout = null;
-	    try {
-	      fout = new FileOutputStream(destinationFile);
-	      for (int c = zin.read(); c != -1; c = zin.read()) {
-    	    fout.write(c);
-    	  }
-    	  zin.closeEntry();
-    	  fout.close();
-	    } catch (IOException e) {
-	      e.printStackTrace();
-	    }
-
-	  }
-
-	  private static void createParentFolder(File destinationFile) {
-	    File parent = new File(destinationFile.getParent());
-	    parent.mkdirs();
-	  }
-	
-	//---------------------------------
-	
 	public static String saveZipToDisk(String saveTo, InputStream inputStream, CustomRequest customRequest) 
 			throws FileNotFoundException, IOException {
 		String pathToZip = saveTo + customRequest.getUsername() + "_" + customRequest.getIdMail() + ".zip";
@@ -77,102 +34,45 @@ public class ZipArchive {
 		return pathToZip;
 	}
 	
-	public static void unzipToDisk(File fileZip, CustomRequest customRequest)
-	{
-		//File f= new File("D:\\zcs\\ancuta_263.zip");
-		String fName = "D:\\zcs\\ancuta_263.zip";
-	    byte[] buf = new byte[1024];
-	    ZipInputStream zinstream = null;
-		try {
-			zinstream = new ZipInputStream(
-			    new FileInputStream(fName));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	    ZipEntry zentry = null;
-		try {
-			zentry = zinstream.getNextEntry();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	    System.out.println("Name of current Zip Entry : " + zentry + "\n");
-	    while (zentry != null) {
-	      String entryName = zentry.getName();
-	      System.out.println("Name of  Zip Entry : " + entryName);
-	      FileOutputStream outstream = null;
-		try {
-			outstream = new FileOutputStream("D:\\zcs\\");
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	      int n;
-
-	      try {
-			while ((n = zinstream.read(buf, 0, 1024)) > -1) {
-			    outstream.write(buf, 0, n);
-
-			  }
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	      System.out.println("Successfully Extracted File Name : "
-	          + entryName);
-	      try {
-			outstream.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	      try {
-			zinstream.closeEntry();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	      try {
-			zentry = zinstream.getNextEntry();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	    }
+	public static void unZipToDisk(File archive, CustomRequest customRequest) {
+		//archive = "D:\\zcs\\ancuta_263.zip";
+		//File baseFolder= new File("D:\\zcs\\ancuta_263");
+		File baseFolder= new File("D:\\zcs\\" + customRequest.getUsername() + "_" + customRequest.getIdMail());
+	    FileInputStream fileInputStream;
 	    try {
-			zinstream.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-}
-	    /*ZipInputStream zipInputStream = null;
-	    try {
-	        zipInputStream = new ZipInputStream(new FileInputStream(f));
-	        ZipEntry zipEntry = zipInputStream.getNextEntry();
-	        byte[] buffer = new byte[(int) zipEntry.getSize()];
-	        FileOutputStream fileOutputStream = new FileOutputStream(customRequest.getUsername() + "_" + customRequest.getIdMail());
-	        while ((zipInputStream.read(buffer))>0){
-	            fileOutputStream.write(buffer);
-	        }
-	        fileOutputStream.flush();
-	        fileOutputStream.close();
+	    	fileInputStream = new FileInputStream(archive);
+	    	ZipInputStream zipInputStream = new ZipInputStream(fileInputStream);
+	    	ZipEntry zipEntry = null;
+	    	while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+	    		File destinationFile = new File(baseFolder, zipEntry.getName());
+	    		unpackEntry(destinationFile, zipInputStream);
+	    	}
+	    	zipInputStream.close();
 	    } catch (FileNotFoundException e) {
-	        e.printStackTrace();
+	    	e.printStackTrace();
 	    } catch (IOException e) {
-	        e.printStackTrace();
-	    } finally 
-	    {
-	        if (zipInputStream != null) 
-	        {
-	            try { zipInputStream.close();
-	            } catch (IOException e) {
-	                e.printStackTrace();
-	            }
-	        }
-	    }*/
-	
+	    	e.printStackTrace();
+	    }
+	  }
+
+	  private static void unpackEntry(File destinationFile, ZipInputStream zipInputStream) {
+		  createParentFolder(destinationFile);
+		  FileOutputStream fileOutputStream = null;
+		  try {
+			  fileOutputStream = new FileOutputStream(destinationFile);
+			  for (int i = zipInputStream.read(); i != -1; i = zipInputStream.read()) {
+				  fileOutputStream.write(i);
+			  }
+    	  zipInputStream.closeEntry();
+    	  fileOutputStream.close();
+		  } catch (IOException e) {
+			  e.printStackTrace();
+		  }
+	  }
+
+	  private static void createParentFolder(File destinationFile) {
+		  File parent = new File(destinationFile.getParent());
+		  parent.mkdirs();
+	  }
+
 }
--- ancuta-gheorghe/zcdtest/WEB-INF/src/com/moodmedia/storeportal/zimbra/content/EmailContent.java
+++ ancuta-gheorghe/zcdtest/WEB-INF/src/com/moodmedia/storeportal/zimbra/content/EmailContent.java
@@ -30,9 +30,9 @@ public class EmailContent extends AContent {
 		InputStream inputStream = null;
 		try {
 	    	inputStream = (InputStream)connection.getInputStream();
-	    	//ZipArchive.unzipToDisk(new File(ZipArchive.saveZipToDisk(saveTo, inputStream, customRequest)), customRequest);
-	    	ZipArchive.saveZipToDisk(saveTo, inputStream, customRequest);
-	    	ZipArchive.unzip();		
+	    	ZipArchive.unZipToDisk(new File(ZipArchive.saveZipToDisk(saveTo, inputStream, customRequest)), customRequest);
+	    	//ZipArchive.saveZipToDisk(saveTo, inputStream, customRequest);
+	    	//ZipArchive.unzip();		
 	    } catch (IOException e) {
 	        e.printStackTrace();
 	    }
